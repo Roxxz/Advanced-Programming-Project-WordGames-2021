6b73307e19d5d4762e52b6f31aa893047c9037ab
Laura Malina Benchea <75676144+LauraMalinaBenchea@users.noreply.github.com>
2021-05-14 20:50:11 +0300

    Add files via upload

---
+++ src/sample/StartedPageController.java
@@ -0,0 +1,13 @@
+package sample;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class StartedPageController {
+
+}
